8f88bb272cad23604a1847170c3e416422a17dd3
smail <smailgourmi@gmail.com>
2023-09-01 00:24:21 +0200

    Add type to function parameters

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,9 @@
-
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { sql } from '@vercel/postgres';
  
-export default async function (
-  request: VercelRequest,
-  response: VercelResponse,
-) {
+export default async function (request : VercelRequest,response : VercelResponse) {
   try {
-    const { rows } = await sql`SELECT * FROM operations;`;
+    const { rows } = await sql`SELECT * FROM operations`;
     return response.status(200).json(rows);
   } catch (error) {
     return response.status(500).json({ error });

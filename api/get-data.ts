e19c54434c42f65fbabc1719bf944dc4e403eea5
smail <smailgourmi@gmail.com>
2023-08-31 20:51:05 +0200

    Add status to response  200 and 500

--- api/get-data.ts
+++ api/get-data.ts
@@ -1,4 +1,14 @@
 
-export default async function ( req, res) {
-  res.json("It works");
+import { sql } from '@vercel/postgres';
+ 
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse,
+) {
+  try {
+    const { rows } = await sql`SELECT * FROM operations;`;
+    return response.status(200).json(rows);
+  } catch (error) {
+    return response.status(500).json({ error });
+  }
 }

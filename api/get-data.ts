2777f905b4ed5c82e7bb01ce7c3b66cd77d44bae
smail <smailgourmi@gmail.com>
2023-08-31 20:22:12 +0200

    go back to base api

--- api/get-data.ts
+++ api/get-data.ts
@@ -1,5 +1,4 @@
-import { sql } from '@vercel/postgres';
+
 export default async function ( req, res) {
-  const { rows } = await sql`SELECT * FROM operations;`;
-  res.json(rows);
+  res.json("It works");
 }
